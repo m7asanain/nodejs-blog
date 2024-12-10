f66c867b32ffe6f877350029989fa491c1397508
Mustafa Hasanain <m7asanain@gmail.com>
2024-12-10 21:01:24 +0300

    set up and use ejs templates

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,33 @@ const router = express.Router();
 
 // routes
 router.get('', (req, res) => {
-    res.send("Hello World");
+    const locals = {
+        title: "NodeJS Blog",
+        description: "Simple Blog created with NodeJs, Express and MongoDB."
+    }
+
+    // res.send("Hello World");
+    res.render('index', { locals });
+});
+
+router.get('/about', (req, res) => {
+    const locals = {
+        title: "About",
+        description: "About Page description."
+    }
+
+    // res.send("Hello World");
+    res.render('about', { locals });
+});
+
+router.get('/contact', (req, res) => {
+    const locals = {
+        title: "Contact",
+        description: "Contact Page description."
+    }
+
+    // res.send("Hello World");
+    res.render('contact', { locals });
 });
 
 module.exports = router;
